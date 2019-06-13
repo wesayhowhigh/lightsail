8580a9d2a4cae5a03dbc613c049aa06be40184d5
Benjamin Jackson <benjacksondev@outlook.com>
2019-06-13 13:21:22 +0100

    Update

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -7,10 +7,10 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 # Add docker's package signing key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # Add repository
-sudo add-apt-repository -y --allow-unauthenticated "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 # Install latest stable docker stable version
 sudo apt-get update
-sudo apt-get -y --allow-unauthenticated install docker-ce
+sudo apt-get -y install docker-ce
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
